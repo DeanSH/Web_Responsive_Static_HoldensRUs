4ac11054bf03e4db7597e9e50b53b76d024b5322
Dean Stanley-Hunt <30317335+DeanSH@users.noreply.github.com>
2019-11-17 14:33:35 +1300

    First Commit

---
+++ Holdens R Us/js/googleanalytics.js
@@ -0,0 +1,10 @@
+﻿/* 
+	Global site tag (gtag.js) - Google Analytics
+	Created by Dean Stanley-Hunt 11/2017!
+*/
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-109325111-1');
+
